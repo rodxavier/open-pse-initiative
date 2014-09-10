ae88923c633e49cae358312639df1868d1831560
Rod Xavier Bondoc <rod_xavier_14@yahoo.com>
2014-09-10 21:58:37 +0800

    Minor change

--- django_project/django_project/urls.py
+++ django_project/django_project/urls.py
@@ -11,7 +11,6 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
 )
 
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
