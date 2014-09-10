801701f2305875a85252305107982ecd178de33b
Rod Xavier Bondoc <rod_xavier_14@yahoo.com>
2014-09-10 23:37:38 +0800

    settings

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -92,7 +92,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static-dev'),
+    os.path.join(BASE_DIR, 'static_dev'),
 )
 
 # Logging
