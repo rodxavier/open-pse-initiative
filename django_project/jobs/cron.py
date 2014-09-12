a7fd154467f125097c23f2fb5560319571d09519
Rod Xavier Bondoc <rod_xavier_14@yahoo.com>
2014-09-12 11:48:21 +0800

    Update cron

--- django_project/jobs/cron.py
+++ django_project/jobs/cron.py
@@ -1,4 +1,6 @@
 #jobs/cron.py
+import logging
+
 from django.core.management import call_command
 
 import kronos
@@ -6,7 +8,11 @@ import kronos
 @kronos.register('0 18 * * *')
 @kronos.register('0 0 * * *')
 def run_daily_job():
-    logger.info('Starting daily_job')
-    call_command('download_quotes')
-    call_command('create_daily_quotes')
-    logger.info('Finished daily_job')
+    try:
+        logger = logging.getLogger('jobs.management.commands')
+        logger.info('Starting daily_job')
+        call_command('download_quotes')
+        call_command('create_daily_quotes')
+        logger.info('Finished daily_job')
+    except Exception, e:
+        logger.error(e)
