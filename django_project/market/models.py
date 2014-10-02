22ba312ac051b3ed03db5fb86ceff4feb82eabbc
Rod Xavier Bondoc <rod_xavier_14@yahoo.com>
2014-10-02 18:22:54 +0800

    NonTradingDay admin

--- django_project/market/models.py
+++ django_project/market/models.py
@@ -7,3 +7,9 @@ class NonTradingDay(models.Model):
         ordering = ('non_trading_date',)
         verbose_name = 'Non Trading Day'
         verbose_name_plural = 'Non Trading Days'
+        
+    def __unicode__(self):
+        return self.non_trading_date
+        
+    def __str__(self):
+        return self.non_trading_date
