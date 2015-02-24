49d037fe65ee8c636e2736cf68319db8d49085b1
Rod Xavier Bondoc <rod_xavier_14@yahoo.com>
2015-02-24 19:10:24 +0800

    Upgrade to DRF 3

--- django_project/api/serializers/quotes_serializers.py
+++ django_project/api/serializers/quotes_serializers.py
@@ -12,15 +12,6 @@ class QuoteSerializer(serializers.ModelSerializer):
         fields = ('symbol', 'name', 'quote_date', 'price_open', 'price_high', 'price_low',
                     'price_close', 'volume',)
                     
-    @property
-    def data(self):
-        ret_data = super(serializers.ModelSerializer, self).data
-        ret_data = CustomList(ret_data)
-        header = self.fields.keys()
-        ret_data.header = lambda: None
-        setattr(ret_data, 'header', header)
-        return ret_data
-                    
     def get_company_name(self, obj):
         return obj.company.name
         
